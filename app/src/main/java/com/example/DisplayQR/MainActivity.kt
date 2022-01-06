674231a16e443dd50c23a41af7d125f2513ce19b
tohhongxiang123 <tohhongxiang@gmail.com>
2022-01-07 01:08:45 +0800

    feat: Add dominant color detection to check whether image shown is valid

--- app/src/main/java/com/example/DisplayQR/MainActivity.kt
+++ app/src/main/java/com/example/DisplayQR/MainActivity.kt
@@ -1,11 +1,152 @@
 package com.example.DisplayQR
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.graphics.*
+import android.media.Image
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.palette.graphics.Palette
+import com.example.DisplayQR.databinding.ActivityMainBinding
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import android.graphics.BitmapFactory
+
+import android.graphics.ImageFormat
+
+import android.graphics.YuvImage
+
+import android.graphics.Bitmap
+import android.media.Image.Plane
+import java.nio.ByteBuffer
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var imageCapture: ImageCapture? = null
+    private lateinit var outputDirectory: File
+
+    private lateinit var cameraExecutor: ExecutorService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        outputDirectory = getOutputDirectory()
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(this, Constants.REQUIRED_PERMISSIONS, Constants.REQUEST_CODE_PERMISSIONS)
+        }
+
+        binding.btnTakePhoto.setOnClickListener {
+            takePhoto()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == Constants.REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(this, "Permissions not granted by user", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder().build().also {
+                mPreview ->
+                    mPreview.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+            }
+
+            imageCapture = ImageCapture.Builder().build()
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+            } catch (e: Exception) {
+                Log.d(Constants.TAG, "Start Camera Failed:", e)
+            }
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun allPermissionsGranted() = Constants.REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun getOutputDirectory() : File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let { mFile ->
+            File(mFile, resources.getString(R.string.app_name)).apply {
+                mkdirs()
+            }
+        }
+
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else filesDir
+    }
+    private fun takePhoto() {
+        val imageCapture = imageCapture ?: return
+
+        imageCapture.takePicture(ContextCompat.getMainExecutor(this), object :
+            ImageCapture.OnImageCapturedCallback() {
+                @SuppressLint("UnsafeOptInUsageError")
+                override fun onCaptureSuccess(image: ImageProxy) {
+//                    Toast.makeText(this@MainActivity, "WOWOWOW", Toast.LENGTH_LONG).show()
+                    val myBitmap = decodeBitmap(image)
+                    val palette = Palette.from(myBitmap!!).generate()
+
+                    val default = 0x000000
+                    val vibrant = palette.getVibrantColor(default).toLong()
+                    val hexString = java.lang.String.format("#%08X", (0xFFFFFFFF and vibrant))
+
+                    val red = hexString.substring(3, 5).toLong(16)
+                    val green = hexString.substring(5, 7).toLong(16)
+                    val blue = hexString.substring(7).toLong(16)
+
+                    val message = if (green > blue && green > red) "valid" else "invalid"
+
+                    Toast.makeText(this@MainActivity, "$red $green $blue $message $hexString", Toast.LENGTH_LONG).show()
+                    image.close()
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Log.e(Constants.TAG, "onError: ${exception.message}", exception)
+                }
+        })
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    private fun decodeBitmap(image: ImageProxy): Bitmap? {
+        val buffer = image.planes[0].buffer
+        val bytes = ByteArray(buffer.capacity()).also { buffer.get(it) }
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
     }
 }
